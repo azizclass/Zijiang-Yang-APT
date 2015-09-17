398744fadf7dda96ba20ee5f8758d685aacd507b
Josh <yangzijiangjosh@gmail.com>
2015-09-17 00:58:22 -0500

    Set up the template

--- connexus/Source/FrontEnd/management.py
+++ connexus/Source/FrontEnd/management.py
@@ -1,6 +1,20 @@
 from google.appengine.api import users
 
 import webapp2
+import jinja2
+import os
+import  urls
+
+path = os.path.dirname(__file__)
+path = os.path.dirname(path)
+path = os.path.dirname(path)
+path = path + '\html_templates'
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(path),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+template_name = 'management.html'
+
 
 class ManagementPage(webapp2.RequestHandler):
     def get(self):
@@ -11,4 +25,8 @@ class ManagementPage(webapp2.RequestHandler):
             self.response.write('Hello, ' + user.nickname())
         else:
             self.redirect(users.create_login_url(self.request.uri))
+
+        template_values = urls.getUrlDir()
+        template = JINJA_ENVIRONMENT.get_template(template_name)
+        self.response.write(template.render(template_values))
 
--- connexus/Source/FrontEnd/urls.py
+++ connexus/Source/FrontEnd/urls.py
@@ -1,9 +1,22 @@
 #Definition of urls of different pages
 URL_MANAGEMENT_PAGE = "/manage"
 URL_LOGIN_PAGE = "/login"
-URL_CREATE_STREAM_PAGE = "/create_stream.py"
+URL_CREATE_STREAM_PAGE = "/create_stream"
 URL_VIEW_STREAM_PAGE = "/view_stream"
 URL_VIEW_ALL_STREAMS_PAGE = "/view_all_streams"
 URL_SEARCH_STREAM_PAGE = "/search_stream"
 URL_TRENDING_STREAMS_PAGE = "/trending_streams"
 URL_ERROR_PAGE = "/error"
+
+def getUrlDir():
+    dic = {
+        'management_addr': URL_MANAGEMENT_PAGE,
+        'login_addr': URL_LOGIN_PAGE,
+        'create_stream_addr': URL_CREATE_STREAM_PAGE,
+        'view_stream_addr': URL_VIEW_STREAM_PAGE,
+        'view_all_streams_addr': URL_VIEW_ALL_STREAMS_PAGE,
+        'search_stream_addr': URL_SEARCH_STREAM_PAGE,
+        'trending_streams_addr': URL_TRENDING_STREAMS_PAGE,
+        'error_addr': URL_ERROR_PAGE
+    }
+    return dic
