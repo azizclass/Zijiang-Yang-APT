0e136107c91ff47b9beada975275afc4461aed0c
Josh <yangzijiangjosh@gmail.com>
2015-10-08 22:10:00 -0500

    Change view stream UI

--- connexus/assets/javascripts/view_stream.js
+++ connexus/assets/javascripts/view_stream.js
@@ -1,8 +1,9 @@
 $(function(){
     $('.light_gallery').lightGallery({
-        selector: '.gallery_item'
+        selector: '.gallery_item',
+        mode: 'lg-fade'
     });
     $('.slider').unslider({
-        delay: false
+        dots: true
     });
 });
