daa6a84458f9090b577a7656a658bba03885577b
Josh <yangzijiangjosh@gmail.com>
2015-10-13 01:58:44 -0500

    Change stream words

--- connexus/Source/FrontEnd/view_all_streams.py
+++ connexus/Source/FrontEnd/view_all_streams.py
@@ -13,7 +13,7 @@ template_name = 'view_all_streams.html'
 class ViewAllStreamsPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        streams = Stream.query(Stream.user == user.email(), ancestor=getStreamKey()).order(-Stream.last_newpic_time,
+        streams = Stream.query(ancestor=getStreamKey()).order(-Stream.last_newpic_time, -Stream.viewCount,
                                -Stream.pic_num, Stream.name, -Stream.create_time)
         template_dict = urls.getUrlDir()
         template_dict['cur_page'] = 'view_all_stream'
