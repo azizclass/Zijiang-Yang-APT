f0bea11ef14a96a4657484ff270b99d055a92fab
Josh <yangzijiangjosh@gmail.com>
2015-11-12 23:29:58 -0600

    Fix the bug to pass tests

--- Lab3/Experiment.py
+++ Lab3/Experiment.py
@@ -4,7 +4,7 @@ def largest(list):
   if len(list) == 0:
     raise ValueError("Cannot call largest on empty list")
   # max = -sys.maxint # "smallest" possible int
-  max = 0
+  max = -sys.maxint-1
   for index in range(len(list)):
     if (list[index] > max):
       max = list[index]
