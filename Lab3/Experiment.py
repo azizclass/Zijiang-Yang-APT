16661b678a4b0f6018c79b36baae9f599bf3ad72
Josh <yangzijiangjosh@gmail.com>
2015-11-13 00:21:38 -0600

    Finally passes

--- Lab3/Experiment.py
+++ Lab3/Experiment.py
@@ -3,7 +3,7 @@ import sys
 def largest(list):
   if len(list) == 0:
     raise ValueError("Cannot call largest on empty list")
-  max = -sys.maxint # "smallest" possible int
+  max = -sys.maxint-1 # "smallest" possible int
   for index in range(len(list)):
     if (list[index] > max):
       max = list[index]
